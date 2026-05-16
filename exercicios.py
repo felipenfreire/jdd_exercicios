7987789f73d4d9f489f8afa718e478b81383929f
Felipe Nascimento <felipenascimento871@gmail.com>
2026-05-16 00:32:23 -0300

    resolvido exercício 2 sobre INT

--- exercicios.py
+++ exercicios.py
@@ -9,6 +9,15 @@ soma =  valor1 + valor2
 print(f'A soma de {valor1} e {valor2} é: {soma}.')
 
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+valor = int(input('Digite qualquer valor: '))
+
+resto = valor % 5
+
+print(f'O resto da divisão de {valor} por 5 é: {resto}')
+
+
+
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
