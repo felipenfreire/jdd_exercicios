eaa93f9bd3f2c28f4d935c7e8a9a86dc8660cced
Felipe Nascimento <felipenascimento871@gmail.com>
2026-05-17 00:11:53 -0300

    resolvido ecercício 6 sobre FLOAT.

--- exercicios.py
+++ exercicios.py
@@ -41,6 +41,12 @@ print(f'O {valor1} ao quadrado é: {quadrado}.')
 # #### Números de Ponto Flutuante (`float`)
 
 # 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
+valor1 = float(input('Digite qualquer valor: '))
+valor2 = float(input('Digite qualquer valor: '))
+
+soma = valor1 + valor2
+
+print(f'A soma entre {valor1} e {valor2} é: {soma}')
 # 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
