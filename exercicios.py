bf9936a3760deffdcfb784b3b01a6fcaf7225241
Felipe Nascimento <felipenascimento871@gmail.com>
2026-05-17 00:35:52 -0300

    resolvido exercício 9 sobre FLOAT.

--- exercicios.py
+++ exercicios.py
@@ -65,6 +65,10 @@ potencia = numero ** expoente
 print(f'A potência de {numero} sobre {expoente} é {potencia}')
 
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+temperatura = float(input('Digite uma temperatura'))
+fahrenheit = (temperatura * 9/5) + 32
+
+print(f'A temperatura: {temperatura} convertida em Fahrenheit é:{fahrenheit}')
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
 
 # #### Strings (`str`)
