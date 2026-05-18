8859175248e2c2049812100714fe6771cc9087ed
Felipe Nascimento <felipenascimento871@gmail.com>
2026-05-18 11:18:00 -0300

    resolvido exercício 14 sobre STR.

--- exercicios.py
+++ exercicios.py
@@ -85,10 +85,19 @@ print(f'{nome.upper()}')
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas
 nome = input('Digite seu nome completo: ')
 print(f'{nome.lower()}')
+
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 frase = input('Digite uma frase que você tem que lembrar dela todos os dias: ')
 print(f'{frase.strip()}')
+
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+data = input('Digite sua idade no formato "dd/mm/aaaa": ')
+
+dia, mes, ano = data.split('/')
+
+print('Dia:', dia)
+print('Mês:', mes)
+print('Ano:', ano)
 
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
